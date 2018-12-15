d9384a32982258f89ebb3db75d6fe1078a34ce1d
Andrew Levandowski <andrewnlevandowski@gmail.com>
2018-12-14 22:40:57 -0800

    Added sword swinging animation

---
+++ Battle_Bot_Royale/Assets/Prefabs/PlayerAttackPrefab/sword_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword_script : MonoBehaviour {
+
+    Animator anim;
+    int swingHash = Animator.StringToHash("isSwinging");
+    float lastSwing = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //!anim.GetCurrentAnimatorStateInfo(0).IsName("sword_swing")
+        if (Input.GetMouseButtonDown(0) && (lastSwing + 1f) < Time.time)
+        {
+            anim.SetTrigger(swingHash);
+            lastSwing = Time.time;
+        }
+    }
+}

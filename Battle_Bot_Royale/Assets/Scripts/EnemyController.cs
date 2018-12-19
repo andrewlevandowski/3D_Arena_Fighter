c891e1e38490a6b2c1a7e73dbd5f563c1f52eb48
Andrew Levandowski <andrewnlevandowski@gmail.com>
2018-12-18 22:02:11 -0800

    added texture to arena, added deaths to bot and player

---
+++ Battle_Bot_Royale/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+//enemy pathfinding from Brackeys https://youtu.be/xppompv1DBg
+public class EnemyController : MonoBehaviour {
+
+    public float lookRadius = 10f;
+
+    public bool justAttacked = false;
+
+    Transform target;
+    NavMeshAgent agent;
+    Animator anim;
+
+    
+    int attackHash = Animator.StringToHash("Attack1");
+    int walkHash = Animator.StringToHash("StartWalk");
+
+    // Use this for initialization
+    void Start () {
+        target = GameSingleton.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector2.Distance(target.position, transform.position);
+        //follows if within range
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            //attacks if close enough, goes back to "walking" if not in close range
+            if (distance <= 2.8f)
+            {
+                FaceTarget();
+                Attack();
+            } else 
+            {
+                anim.SetTrigger(walkHash);
+                justAttacked = false;
+
+            }
+        }
+	}
+
+    //turns robot smoothly towards person
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+
+    void Attack()
+    {
+
+        anim.SetTrigger(attackHash);
+        justAttacked = true;
+    }
+
+}
---
+++ Battle_Bot_Royale/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+//outline taken from: "https://unity3d.com/learn/tutorials/projects/survival-shooter/player-health?playlist=17144"
+public class PlayerHealth : MonoBehaviour {
+
+    /*
+     *TODO: GET RID OF COMMENTS ON PLAYER ATTACK AND MOVEMENT WHEN THOSE SCRIPTS ARE WRITTEN 
+     */
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public AudioClip punchSound1;
+    public AudioClip punchSound2;
+    public AudioClip punchSound3;
+    public AudioClip punchSound4;
+    public AudioClip punchSound5;
+
+    public AudioClip deathSound;
+    //may change this to an integer and just display it on screen
+    public Slider healthSlider;
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    AudioSource playerAudio0;
+    CharacterController playerMovement;
+    
+
+    System.Random rnd = new System.Random();
+    //playerAttack playerAttack;
+    bool isDead;
+    bool damaged;
+    int playerDeadHash = Animator.StringToHash("PlayerDead");
+
+    private void Awake()
+    {
+        AudioSource[] sources = GetComponents<AudioSource>();
+        playerAudio0 = sources[0];
+        playerAudio = sources[1];
+        playerAudio.volume = 0.4f;
+        playerMovement = GetComponent<CharacterController>();
+        //playerAttack = GetComponent<PlayerAttack>();
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        //healthSlider.value = currentHealth;
+
+        //set sound to death clip so hurt animation doesn't play
+        //playerAudio.clip = deathClip;
+        int iCase = rnd.Next(1, 6);
+        switch (iCase)
+        {
+            case 1:
+                playerAudio.clip = punchSound1;
+                break;
+            case 2:
+                playerAudio.clip = punchSound2;
+                break;
+            case 3:
+                playerAudio.clip = punchSound3;
+                break;
+            case 4:
+                playerAudio.clip = punchSound4;
+                break;
+            case 5:
+                playerAudio.clip = punchSound5;
+                break;
+            default:
+                playerAudio.clip = punchSound5;
+                break;
+        }
+
+
+        
+        playerAudio.Play();
+
+        if (currentHealth <= 0 && isDead == false)
+        {
+            isDead = true;
+            GameSingleton.instance.playerDead = true;
+            playerAudio0.clip = deathSound;
+            GetComponent<FirstPersonController>().enabled = false;
+            anim.SetTrigger(playerDeadHash);
+            playerAudio0.Play();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            playerMovement.enabled = false;
+
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Enemy")
+            TakeDamage(1);
+    }
+}
